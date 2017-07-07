bf2466183cbe3200b440339a49f0a15150515485
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-07 17:44:20 +0900

    4th

--- src/com/javaex/problem04/CConverterApp.java
+++ src/com/javaex/problem04/CConverterApp.java
@@ -1,22 +1,21 @@
 package com.javaex.problem04;
 
-
 public class CConverterApp {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
-        double dollar;
-        double won;
-        
-        CConverter.setRate(1118.70);
-        
-        
-        //백만원을 달러로 출력
+		double dollar;
+		double won;
 
-        
-        //100달려를 원으로 출력
-        
-        
-    }
+		CConverter.setRate(1118.70);
+
+
+		dollar = CConverter.toDoller(1000000);
+		System.out.println("백만원은 " + dollar + "입니다.");
+
+		won = CConverter.toKWR(100);
+		System.out.println("백달러는 " + won + "입니다.");
+
+	}
 
 }
